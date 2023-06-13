984124d16dc2910fd60611f6310f573ed9e7ec19
damjanPetr <pedamovski@gmail.com>
2023-06-13 20:30:56 +0200

    create index and page2

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <div class="homepage">
+        <div class="">
+            <div class="hero-content">
+                <h1>Create your very own website</h1>
+                <h2>All you need is to fill up a form !</h2>
+                <a href="./page2.php">Start</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ page2.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <div class="homepage ">
+        <main class="column-area">
+            <form action="post">
+                <div class="column">
+                    <div class="">
+                        <p>Provide a description of your company, something people should be aware of before they contact you:</p>
+                    </div>
+                    <div class="">
+                        <label for="coverImg">Cover Img Url</label>
+                        <input type="text" id="coverImg" name="coverImg">
+                    </div>
+                    <div class="">
+                        <label for="mainPageTitle">Main Title Of Page</label>
+                        <input type="text" id="mainPageTitle" name="mainPageTitle">
+                    </div>
+                    <div class="">
+                        <label for="subtitleOfPage">Subtitle Of page</label>
+                        <input type="text" id="subtitleOfPage" name="subtitleOfPage">
+                    </div>
+                    <div class="">
+                        <label for="about">Something About You</label>
+                        <textarea id="about" name="about"></textarea>
+                    </div>
+                    <div class="">
+                        <label for="telNumebr">Your telephone Number</label>
+                        <input type="tel" id="telNumebr" name="telNumebr">
+                    </div>
+                    <div class="">
+                        <label for="Location">Location</label>
+                        <input type="text" id="Location" name="Location">
+                    </div>
+                    <div class="">
+                        <label for="services">Do you provide services or producs?</label>
+                        <select id="services" name="services">
+                            <option value="services">Services</option>
+                            <option value="services">Products</option>
+
+                        </select>
+                    </div>
+                </div>
+
+
+                <div class="column">
+                    <div class="">
+                        <p>Provide url for an image anad description of your servire/product</p>
+                    </div>
+                    <div class="">
+
+                        <label for="imgUrl">Image URL</label>
+                        <input type="text" id="imgUrl" name="imgUrl">
+                    </div>
+                    <div class="">
+                        <label for="descService">Description of service/product</label>
+                        <input type="text" id="descService" name="descService">
+                    </div>
+                    <div class="">
+                        <label for="imgUrl">Image URL</label>
+                        <input type="text" id="imgUrl" name="imgUrl">
+                    </div>
+                    <div class="">
+                        <label for="descService">Description of service/product</label>
+                        <input type="text" id="descService" name="descService">
+                    </div>
+                    <div class="">
+                        <label for="imgUrl">Image URL</label>
+                        <input type="text" id="imgUrl" name="imgUrl">
+                    </div>
+                    <div class="">
+                        <label for="descService">Description of service/product</label>
+                        <input type="text" id="descService" name="descService">
+                    </div>
+                </div>
+
+
+
+                <div class="column">
+                    <div class="">
+                        <p>Provide a description of your company, something people should be aware of before they contact you:</p>
+                    </div>
+                    <div class="">
+                        <label for="linkedin">Linkedin:</label>
+                        <input type="text" id="linkedin" name="linkedin">
+                    </div>
+                    <div class="">
+                        <label for="facebook">facebook</label>
+                        <input type="text" id="facebook" name="facebook">
+                    </div>
+                    <div class="">
+                        <label for="twitter">Twitter:</label>
+                        <input type="text" id="twitter" name="twitter">
+
+                    </div>
+                    <div class="">
+                        <label for="instagram">Instagram</label>
+                        <input type="text" id="instagram" name="instagram">
+                    </div>
+                </div>
+            </form>
+        </main>
+    </div>
+
+</body>
+
+</html>
