c37b12401424ea645dc750c6cd84cf65ec77ec57
damjanPetr <pedamovski@gmail.com>
2023-06-23 17:02:59 +0200

    add Database class

--- Database/Connection.php
+++ Database/Connection.php
@@ -1,11 +1,48 @@
 
 
-
-
 <?php
 
-try {
-} catch (\Throwable $e) {
-    var_dump($e);
-} ?>
+namespace Database;
 
+class Connection
+{
+
+    const HOST = 'localhost:3307';
+    const DB_NAME = 'challenge17';
+    const USERNAME = 'root';
+    const PASSWORD = '';
+
+
+    protected $connection;
+    public function __construct()
+    {
+        try {
+            $this  = new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::DB_NAME, self::USERNAME, self::PASSWORD);
+        } catch (\Throwable $e) {
+            var_dump($e);
+        }
+    }
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+
+    public function __destruct()
+    {
+        $this->connection = null;
+    }
+    public function lastInsertId()
+    {
+        return $this->connection->lastInsertId();
+    }
+    public function query($sql)
+    {
+
+        $query = $this->connection->prepare($sql);
+        $query->execute();
+        return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
+
+?>
