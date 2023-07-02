2e412538860da732a657cf542c91732c0e4891d5
damjanPetr <pedamovski@gmail.com>
2023-07-02 23:21:30 +0200

    result

--- file.js
+++ file.js
@@ -1,3 +1,55 @@
-if (number % 2 === 0) {
-  console.log(first);
+// 1.
+
+const testNumber = 10;
+
+if (testNumber % 2 === 0) {
+  console.log("The number you write is even");
+} else {
+  console.log("The number you write is not even");
 }
+
+// 2.
+
+for (let i = 10; i <= 100; i++) {
+  if (i % 2 === 0 && i % 3 === 0) {
+    console.log(i);
+  }
+}
+
+// 3.
+
+const testPrimeNumber1 = 10;
+const testPrimeNumber2 = 5;
+const testPrimeNumber3 = 13;
+
+const largestNumber = Math.max(
+  testPrimeNumber1,
+  testPrimeNumber2,
+  testPrimeNumber3
+);
+const smallestNumber = Math.min(
+  testPrimeNumber1,
+  testPrimeNumber2,
+  testPrimeNumber3
+);
+
+console.log(`Largest number is ${largestNumber} `);
+console.log(`Smallest number is ${smallestNumber}`);
+
+function testPrime(num) {
+  if (num <= 1) {
+    console.log("1 is not prime number");
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      console.log(`${num} is not a prime number`);
+      return;
+    } else {
+      console.log(`${num} is a prime number`);
+      return;
+    }
+  }
+}
+
+testPrime(largestNumber);
+testPrime(smallestNumber);
